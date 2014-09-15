c62b0f05f844f946be591eb91164b7bf15fb026c
ssoti2201 <ssoti2201@bvsd.org>
2014-09-14 23:19:56 -0600

    more minor changes

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 //hovering menu
 $(document).ready(function() {
-	$("#home_nav td, #home_silde_toggle").hover(
+	$("#home_nav td, #home_slide_toggle").hover(
 		function(){ 
-			var id = $(this).attr('id').split('-')[1];
 			var style = {
-				display: "inline-block",
+				display: "block",
 				margin: "-8px 0px"
 			}
-			$(".home-" + id).css(style); }, 
-		function() {
 			var id = $(this).attr('id').split('-')[1];
+			$(".home-" + id).css(style);
+			var offset = $(this).offset(); 
+			$(".home-"+ id).offset({left: offset.left});
+		}, 
+		function() {
 			var style = {
 				display: "none",
 			}
+			var id = $(this).attr('id').split('-')[1];
 			$(".home-" + id).css(style);
 		}
 	);
